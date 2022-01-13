237bffccbb6ed6abd595eefb86c0339974970ee8
lavi-chadha <lavi.chadha0109@gmail.com>
2022-01-13 15:42:21 +0530

    first

---
+++ first.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Free responsive business website template</title>
+  <link rel="stylesheet" href="css/components.css">
+  <link rel="stylesheet" href="css/icons.css">
+  <link rel="stylesheet" href="css/responsee.css">
+  <link rel="stylesheet" href="owl-carousel/owl.carousel.css">
+  <link rel="stylesheet" href="owl-carousel/owl.theme.css">
+  <!-- CUSTOM STYLE -->
+  <link rel="stylesheet" href="css/template-style.css">
+  <link rel="stylesheet" href="css/new.css">
+  <link href="https://fonts.googleapis.com/css?family=Barlow:100,300,400,700,800&amp;subset=latin-ext" rel="stylesheet">
+  <script type="text/javascript" src="js/jquery-1.8.3.min.js"></script>
+  <script type="text/javascript" src="js/jquery-ui.min.js"></script>
+  <script type="text/javascript" src="js/validation.js"></script>
+</head>
+
+<!--
+    You can change the color scheme of the page. Just change the class of the <body> tag. 
+    You can use this class: "primary-color-white", "primary-color-red", "primary-color-orange", "primary-color-blue", "primary-color-aqua", "primary-color-dark" 
+    -->
+
+<!--
+    Each element is able to have its own background or text color. Just change the class of the element.  
+    You can use this class: 
+    "background-white", "background-red", "background-orange", "background-blue", "background-aqua", "background-primary" 
+    "text-white", "text-red", "text-orange", "text-blue", "text-aqua", "text-primary"
+    -->
+
+<body class="size-1520 primary-color-red background-dark">
+
+  <!-- HEADER -->
+  <header class="grid">
+    <!-- Top Navigation -->
+    <nav class="s-12 grid background-none background-primary-hightlight">
+      <!-- logo -->
+      <a href="index.html" class="m-12 l-3 padding-2x logo">
+
+
+      </a>
+      <div class="top-nav s-12 l-9">
+        <ul class="top-ul right chevron">
+          <li><a href="index.html">Home</a></li>
+          <li><a href="about-us.html">LOGIN</a></li>
+          <li><a href="services.html">SIGNUP</a></li>
+          <li><a href="gallery.html">Gallery</a></li>
+          <li><a href="contact.html">Contact</a></li>
+
+        </ul>
+      </div>
+    </nav>
+  </header>
+
+  <br>
+  <br>
+
+
+  <!-- MAIN -->
+  <main role="main">
+    <!-- TOP SECTION -->
+    <section class="grid">
+      <!-- Main Carousel -->
+      <div
+        class="s-12 margin-bottom carousel-fade-transition owl-carousel carousel-main carousel-nav-white carousel-hide-arrows background-dark">
+        <div class="item background-image" style="background-image:url(img/carousel-01.jpg)">
+          <center>
+            <button type="submit"
+              style="height: 45px ;width: 75px; color:darkblue; background-color: burlywood; font-weight: bold;"><i
+                class="fa fa-search">
+                SEARCH</i></button>
+            <br>
+            <br>
+            <form action="/action_page.php">
+              <input type="text" style="height: 45px ;width: 260px" placeholder="Search.." name="search">
+
+            </form>
+          </center>
+        </div>
+        <div class="item background-image" style="background-image:url(img/carousel-02.jpg)">
+          <center>
+            <button type="submit"
+              style="height: 45px ;width: 75px; color:darkblue; background-color: burlywood; font-weight: bold;"><i
+                class="fa fa-search">
+                SEARCH</i></button>
+
+
+
+            <br>
+            <br>
+
+            <form action="/action_page.php">
+              <input type="text" style="height: 45px ;width: 260px" placeholder="Search.." name="search">
+
+
+            </form>
+          </center>
+        </div>
+        <div class="item background-image" style="background-image:url(img/carousel-03.jpg)">
+          <center>
+            <button type="submit"
+              style="height: 45px ;width: 75px; color:darkblue; background-color: burlywood; font-weight: bold;"><i
+                class="fa fa-search">
+                SEARCH</i></button>
+            <br>
+
+            <br>
+            <form action="/action_page.php">
+              <input type="text" style="height: 45px ;width: 260px" placeholder="Search.." name="search">
+
+            </form>
+          </center>
+        </div>
+      </div>
+    </section>
+
+    <br>
+    <br>
+    <section class=" margin-bottom background-dark">
+      <center>
+        <h2 class="s-12 l-6 center text-thin text-size-30 text-white text-uppercase margin-bottom-30">
+          <b>PRODUCTS</b>
+        </h2>
+      </center>
+
+      <center>
+        <p class="s-12 l-6 center">
+          <span>
+            MOBILES
+            LAPTOP
+            BOOKS
+            COLLEGESIt's a good idea to headline each category and to write succinct explanations for each part of the
+            phone; a potential buyer won't linger too long for the advice. Talk about what you like about the smartphone
+            you're reviewing, how easy it is to use, what features in particular are of worthy note.
+          </span>
+        </p>
+      </center>
+    </section>
+    <BR>
+    <BR>
+    <!-- SECTION 1 -->
+    <section class="grid margin text-center">
+
+      <a href="css/components.css" class="s-12 m-6 l-3 padding-2x vertical-center margin-bottom background-red">
+        <br>
+        <img src="img/11.jpg"></img>
+        <h3 class="text-strong text-size-20 text-line-height-1 margin-bottom-30 text-uppercase">MOBILE PHONES
+        </h3>
+      </a>
+      <a href="/" class="s-12 m-6 l-3 padding-2x vertical-center margin-bottom background-blue">
+        <br>
+        <br>
+        <img src="img/22.jfif" class="icon-sli-equalizer text-size-60 text-white center margin-bottom-15"
+          style="height: 300;"></img>
+        <h3 class="text-strong text-size-20 text-line-height-1 margin-bottom-30 text-uppercase">LAPTOP
+        </h3>
+      </a>
+
+      <!-- Image-->
+
+
+      <a href="/" class="s-12 m-6 l-3 padding-2x vertical-center margin-bottom background-orange">
+        <br>
+        <img src="img/55.jfif" class="icon-sli-equalizer text-size-60 text-white center margin-bottom-15"></img>
+        <h3 class="text-strong text-size-20 text-line-height-1 margin-bottom-30 text-uppercase">BOOKS</h3>
+      </a>
+      <a href="/" class="s-12 m-6 l-3 padding-2x vertical-center margin-bottom background-aqua">
+        <br>
+        <img src="img/66.jfif" class="icon-sli-equalizer text-size-60 text-white center margin-bottom-15"></img>
+        <h3 class="text-strong text-size-20 text-line-height-1 margin-bottom-30 text-uppercase">COLLEGES</h3>
+      </a>
+    </section>
+
+    <!-- SECTION 2 -->
+
+
+
+
+    <script type="text/javascript" src="js/responsee.js"></script>
+    <script type="text/javascript" src="owl-carousel/owl.carousel.js"></script>
+    <script type="text/javascript" src="js/template-scripts.js"></script>
+
+</body>
+
+</html>
