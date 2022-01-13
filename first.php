8774d3117f7906c16021bf54e66e843ba21341c6
lavi-chadha <lavi.chadha0109@gmail.com>
2022-01-13 15:43:55 +0530

    HI

--- first.php
+++ first.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Free responsive business website template</title>
+  <title>FIRST</title>
   <link rel="stylesheet" href="css/components.css">
   <link rel="stylesheet" href="css/icons.css">
   <link rel="stylesheet" href="css/responsee.css">
