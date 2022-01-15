c7c25830d68fa4464de32ce1666034fc786c6fca
lavi-chadha <lavi.chadha0109@gmail.com>
2022-01-15 16:45:09 +0530

    15-01-22 by lavi

--- first.php
+++ first.php
@@ -44,9 +44,9 @@
       </a>
       <div class="top-nav s-12 l-9">
         <ul class="top-ul right chevron">
-          <li><a href="index.html">Home</a></li>
-          <li><a href="about-us.html">LOGIN</a></li>
-          <li><a href="services.html">SIGNUP</a></li>
+          <li><a href="#">Home</a></li>
+          <li><a href="acct.php">LOGIN</a></li>
+          <li><a href="signup.php">SIGNUP</a></li>
           <li><a href="gallery.html">Gallery</a></li>
           <li><a href="contact.html">Contact</a></li>
 
