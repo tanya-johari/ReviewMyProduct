cd85e016b450c39326c3a0100f7adcaf96ac00f5
lavi-chadha <lavi.chadha0109@gmail.com>
2022-01-19 16:10:52 +0530

    .

--- FINAL/database/shopee.sql
+++ FINAL/database/shopee.sql
@@ -35,18 +35,18 @@ CREATE TABLE `product` (
 
 INSERT INTO `product` (`item_id`, `item_brand`, `item_name`, `item_price`, `item_image`, `item_register`) VALUES
 (1, 'Samsung', 'Samsung Galaxy 10', 152.00, './assets/products/1.png', '2020-03-28 11:08:57'), -- NOW()
-(2, 'Redmi', 'Redmi Note 7', 122.00, './assets/products/2.png', '2020-03-28 11:08:57'),
-(3, 'Redmi', 'Redmi Note 6', 122.00, './assets/products/3.png', '2020-03-28 11:08:57'),
-(4, 'Redmi', 'Redmi Note 5', 122.00, './assets/products/4.png', '2020-03-28 11:08:57'),
-(5, 'Redmi', 'Redmi Note 4', 122.00, './assets/products/5.png', '2020-03-28 11:08:57'),
-(6, 'Redmi', 'Redmi Note 8', 122.00, './assets/products/6.png', '2020-03-28 11:08:57'),
-(7, 'Redmi', 'Redmi Note 9', 122.00, './assets/products/8.png', '2020-03-28 11:08:57'),
-(8, 'Redmi', 'Redmi Note', 122.00, './assets/products/10.png', '2020-03-28 11:08:57'),
-(9, 'Samsung', 'Samsung Galaxy S6', 152.00, './assets/products/11.png', '2020-03-28 11:08:57'),
-(10, 'Samsung', 'Samsung Galaxy S7', 152.00, './assets/products/12.png', '2020-03-28 11:08:57'),
-(11, 'Apple', 'Apple iPhone 5', 152.00, './assets/products/13.png', '2020-03-28 11:08:57'),
-(12, 'Apple', 'Apple iPhone 6', 152.00, './assets/products/14.png', '2020-03-28 11:08:57'),
-(13, 'Apple', 'Apple iPhone 7', 152.00, './assets/products/15.png', '2020-03-28 11:08:57');
+(2, 'Redmi', 'Redmi Note 7', 122.00, './assets/products/2.png', '2022-01-28 11:08:57'),
+(3, 'Redmi', 'Redmi Note 6', 122.00, './assets/products/3.png', '2022-01-28 11:08:57'),
+(4, 'Redmi', 'Redmi Note 5', 122.00, './assets/products/4.png', '2022-01-28 11:08:57'),
+(5, 'Redmi', 'Redmi Note 4', 122.00, './assets/products/5.png', '2022-01-28 11:08:57'),
+(6, 'Redmi', 'Redmi Note 8', 122.00, './assets/products/6.png', '2022-01-28 11:08:57'),
+(7, 'Redmi', 'Redmi Note 9', 122.00, './assets/products/8.png', '2022-01-28 11:08:57'),
+(8, 'Redmi', 'Redmi Note', 122.00, './assets/products/10.png', '2022-01-28 11:08:57'),
+(9, 'Samsung', 'Samsung Galaxy S6', 152.00, './assets/products/11.png', '2022-01-28 11:08:57'),
+(10, 'Samsung', 'Samsung Galaxy S7', 152.00, './assets/products/12.png', '2022-01-28 11:08:57'),
+(11, 'Apple', 'Apple iPhone 5', 152.00, './assets/products/13.png', '2022-01-28 11:08:57'),
+(12, 'Apple', 'Apple iPhone 6', 152.00, './assets/products/14.png', '2022-01-28 11:08:57'),
+(13, 'Apple', 'Apple iPhone 7', 152.00, './assets/products/15.png', '2022-01-28 11:08:57');
 
 
 ALTER TABLE `product`
