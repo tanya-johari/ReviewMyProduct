f6eab12b8a2bf4d3cb16d6617e838a3cbc907c39
lavi-chadha <lavi.chadha0109@gmail.com>
2022-02-05 15:20:40 +0530

    .

--- FINAL/database/DBController.php
+++ FINAL/database/DBController.php
@@ -7,7 +7,7 @@ class DBController
     protected $host = 'localhost';
     protected $user = 'root';
     protected $password = '';
-    protected $database = "shopee";
+    protected $database = "product_reviewer";
 
     // connection property
     public $con = null;
