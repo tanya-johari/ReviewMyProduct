dcf6c8c389d2532b72105aaedb4e97b4f4b6d4f1
Yes vishakha <vishakhayadav.s2mca21@bvicam.in>
2022-02-22 18:34:00 +0530

    css

--- signup.php
+++ signup.php
@@ -55,12 +55,12 @@ echo $statusMsg;
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/signup.css">
     <title>Signup</title>
     <style>
          body {
           width: 100%;
-            background-image: url("/ReviewMyProduct/img/1.jpg");
+            background-image: url("/ReviewMyProduct/img/4.jpeg");
             background-position: center;
             background-size: cover;
             height: 130vh;
@@ -131,31 +131,34 @@ echo $statusMsg;
     <div class="container">
         <h1 class="text-center" style="color:#ff7200;">Welcome To ReviewMyProduct</h1>
         <form action="/ReviewMyProduct/signup.php" method="post" enctype="multipart/form-data">
-        <div class="col-md-4">
+        <div class="form1">
+        <div class="col-md-5">
         <label style="color:#ff7200;">Select profile photo * :</label>
         <input type="file" name="image" required>
         </div>
-        <div class="col-md-4">
+        <div class="col-md-5">
         <label for="email" class="form-label" style="color:#ff7200;">Email-id * :</label>
         <input type="text" class="form-control" id="email" name=email aria-describedby="emailHelp" placeholder="Enter Email Id Here" required>
         </div>
-        <div class="col-md-4">
+        <div class="col-md-5">
         <label for="name" class="form-label" style="color:#ff7200;">Name * :</label>
         <input type="text" class="form-control" id="name" name="name" placeholder="Enter Name Here" required>
         </div>
-        <div class="col-md-4">
+        <div class="col-md-5">
         <label for="username" class="form-label" style="color:#ff7200;">Username * :</label>
         <input type="text" class="form-control" id="username" name="username" placeholder="Enter UserName Here" required>
         </div>
-        <div class="col-md-4">
+        <div class="col-md-5">
         <label for="phoneno" class="form-label" style="color:#ff7200;">Mobile Number * :</label>
         <input type="tel" class="form-control" id="phoneno" name="phoneno" placeholder="Enter Phone no Here" required>
         </div>
-        <div class="col-md-4">
+    </div>
+    <div class="form2">
+        <div class="col-md-5">
         <label for="dob" class="form-label" style="color:#ff7200;">Date of Birth * :</label>
         <input type="date" class="form-control" id="dob" name="dob" min="1960-01-01" max="2011-12-31" placeholder="Select DOB " required>
         </div>
-        <div class="col-md-4">
+        <div class="col-md-5">
         <label for="gender" class="form-label" style="color:#ff7200;">Gender (optional) :</label>
         <select id="gender" name="gender" class="form-select">
         <option selected>selected</option>
@@ -163,22 +166,24 @@ echo $statusMsg;
         <option value="female">Female</option>
         </select>
         </div>
-        <div class="col-md-4">
+        <div class="col-md-5">
         <label for="password" class="form-label" style="color:#ff7200;">Password * :</label>
         <input type="password" class="form-control" id="password" name="password" placeholder="Enter Password Here" required>
         </div>
-        <div class="col-md-4">
+        <div class="col-md-5">
         <label for="cpassword" class="form-label" style="color:#ff7200;">Confirm Password * :</label>
         <input type="password" class="form-control" id="cpassword" name="cpassword" placeholder="Confirm password" required>
         <div id="emailHelp" class="form-text">Make sure to enter the same Password.</div>
         </div>
         <br>
-        <div class="col-md-4">
+        <br>
+    </div>
+        <div class="col-md-5">
         <input type="checkbox" class="form-check-input" id="exampleCheck1" style="color:#ff7200;">
         <label class="form-check-label" for="exampleCheck1" style="color:#ff7200;">All Entered Data is correct</label>
         </div>
         <br>
-        <button type="submit" class="btn btn-primary">Sign Up</button>
+        <button type="submit" class="btn btn-primary" style="color:#ffffff;">Sign Up</button>
         </form>
     </div>
 
