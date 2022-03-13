bbbec90e7e1467f92dc1bd02640fe75a98fd2656
Tanya <johtanya11@gmail.com>
2022-03-13 15:06:33 +0530

    chng

--- FINAL/footer.php
+++ FINAL/footer.php
@@ -35,6 +35,15 @@
                 </div>
             </div>
         </div>
+        </br>
+<h5 align="center">
+© <?php echo date("Y"); ?> 
+</br>
+Made by : <a href="https://github.com/tanya-johari" style="color:orange;"> Tanya Johari </a>
+<a href="https://github.com/YesVishakha" style="color:yellow;"> Vishakha Yadav </a>
+<a href="https://github.com/lavi-chadha" style="color:lightblue;"> Lavi Chadha </a>
+</br>
+</br>
     </div>
 </footer>
 
@@ -53,13 +62,5 @@
 <!-- Custom Javascript -->
 <script src="index.js"></script>
 </body>
-</br>
-<h5 align="center">
-© <?php echo date("Y"); ?> 
-</br>
-Made by : <a href="https://github.com/tanya-johari" style="color:orange;"> Tanya Johari </a>
-<a href="https://github.com/YesVishakha" style="color:red;"> Vishakha Yadav </a>
-<a href="https://github.com/lavi-chadha" style="color:blue;"> Lavi Chadha </a>
-</br>
-</br>
+
 </html>
