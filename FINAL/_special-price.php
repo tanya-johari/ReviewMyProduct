c6b638abbc1e4ed95eab8679b3d4cc523f4201c6
Tanya <johtanya11@gmail.com>
2022-03-14 16:28:20 +0530

    img size

--- FINAL/_special-price.php
+++ FINAL/_special-price.php
@@ -54,7 +54,7 @@ $in_cart =getproduct();
                 <div class="item py-2" style="width:20px height:20px; ">
                     <div class="product font-rale">
                         
-                        <a href="products.php?itemid=<?php echo $item['item_id']; ?>&itemname=<?php echo $item['item_name']; ?>"><img src="<?php echo $item['item_image'] ?? "./assets/products/13.png"; ?>" alt="product1" width="200px" height="200px" class="img-fluid" style="object-fit: cover;"></a>
+                        <a href="products.php?itemid=<?php echo $item['item_id']; ?>&itemname=<?php echo $item['item_name']; ?>"><img src="<?php echo $item['item_image'] ?? "./assets/products/13.png"; ?>" alt="product1" width="180px" height="180px"  object-fit= "cover"></a>
                         
                         <div class="text-center">
                             <h6><?php echo $item['item_name'] ?? "Unknown"; ?></h6>
