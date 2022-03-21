fafcda29ee586f7de865cdc16c41def59449e8e4
lavi-chadha <lavi.chadha0109@gmail.com>
2022-03-21 13:10:28 +0530

    1

--- FINAL/products.php
+++ FINAL/products.php
@@ -95,6 +95,7 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
 <h4 style="color:#311CA6; "><?php echo $row2['item_brand']; ?></h4></br></div>
 </center>
 </div>
+
 <div class=f1>
   <h3 style="color:#336296">SPECFICATIONS:</h3>
 <h5><?php echo $row2['description']; ?></h5></br>
